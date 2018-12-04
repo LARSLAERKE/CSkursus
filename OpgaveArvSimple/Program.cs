6efdef495c0126f46a9d709b1d42e3cedae7f2b6
Lars Lærke Petersen <larspe@danskebank.dk>
2018-12-04 15:50:04 +0100

    Færdig dag 4

---
+++ OpgaveArvSimple/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveArvSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Person
+    {
+
+    }
+
+}
